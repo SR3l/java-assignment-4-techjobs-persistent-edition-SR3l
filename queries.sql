ccef6b6d6b1ced26e84bf2d260b0b9ea8b74292b
SR3I <d.alwoan@yahoo.com>
2023-02-24 11:12:43 -0800

    Last one

--- queries.sql
+++ queries.sql
@@ -1,7 +1,12 @@
 -- Part 1: Test it with SQL
-
+id INTEGER
+name VARCHAR
+employer VARCHAR
+skills VARCHAR
 -- Part 2: Test it with SQL
-
+Select name FROM employer WHERE location = "St. Louis City"
 -- Part 3: Test it with SQL
-
+DROP TABLE job
 -- Part 4: Test it with SQL
+
+SELECT * FROM skill LEFT JOIN job ON skill.id = job.id ORDER BY skill.name ASC
--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,51 +1,41 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-public class Job{
+public class Job extends AbstractEntity{
+    @ManyToOne
+    private Employer employer;
 
-    @Id
-    @GeneratedValue
-    private int id;
-
-    private String name;
-
-    private String employer;
-    private String skills;
+    @ManyToMany
+    private List<Skill> skills = new ArrayList<>();
+    public List<Skill> getSkills() {
+        return skills;
+    }
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
 
     public Job() {
     }
 
-    public Job(String anEmployer, String someSkills) {
+    public Job(Employer someEmployer, List<Skill> someSkills) {
         super();
-        this.employer = anEmployer;
-        this.skills = someSkills;
+        //this.employer = someEmployer;
+        //this.skills = someSkills;
     }
 
     // Getters and setters.
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
+    public Employer getEmployer(){
         return employer;
     }
-
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
-    public String getSkills() {
-        return skills;
-    }
 
-    public void setSkills(String skills) {
-        this.skills = skills;
-    }
 }
+
