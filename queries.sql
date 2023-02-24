46e4662bbce8b1a9752ae13568c23fc3a53146ca
SR3I <d.alwoan@yahoo.com>
2023-02-24 12:58:29 -0800

    Final

--- queries.sql
+++ queries.sql
@@ -8,5 +8,7 @@ Select name FROM employer WHERE location = "St. Louis City"
 -- Part 3: Test it with SQL
 DROP TABLE job
 -- Part 4: Test it with SQL
-
-SELECT * FROM skill LEFT JOIN job ON skill.id = job.id ORDER BY skill.name ASC
+SELECT * FROM skill
+      LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+      WHERE job_skills.jobs_id IS NOT NULL
+      ORDER BY name ASC;
